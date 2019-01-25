b2cf4a916b89ff6e1de7dd1cdaca93aa18940916
LMongoose <33644529+LMongoose@users.noreply.github.com>
2019-01-25 13:28:56 -0200

    Add files via upload

--- virustotalAPI.py
+++ virustotalAPI.py
@@ -1,11 +1,16 @@
-﻿# Original author: Lucas Soares Pellizzaro on 2018-12-11
+﻿# Original author: Lucas Soares Pellizzaro on 2019-01-24
 
-import re
+import time, re
 
+# Constants
 _CONNECTION = None
+_USERAGENT = ""
+_INTERVAL = 1
 _PROXYURL = ""
 _APIKEY = ""
+_BASE_APIURL = "https://www.virustotal.com/vtapi/v2/"
 
+# Exceptions
 class ConnectionError(Exception):
 	"""
 	Exception raised by connection not started properly.
@@ -13,12 +18,14 @@ class ConnectionError(Exception):
 		message -- explanation of the error
 	"""
 	def __init__(self):
-		self.msg1 = "A connection has not been set,"
-		self.msg2 = " use 'startConnection()' to start a connection"
-		self.msg3 = " and 'setProxy(proxy_url)' if you are using a proxy."
-		self.message = self.msg1+self.msg2+self.msg3
+		_msg1 = "A connection has not been set,"
+		_msg2 = " use 'startConnection()' to start a connection"
+		_msg3 = " and 'setProxy(proxy_url)' if you are using a proxy."
+		self.message = _msg1+_msg2+_msg3
 
+# Local functions
 def _sendArtifact(p_baseurl, p_artifact):
+	time.sleep(_INTERVAL)
 	# Starts the request
 	request_params = {
 		"apikey": _APIKEY,
@@ -45,14 +52,16 @@ def startConnection():
 	import urllib3
 	urllib3.disable_warnings()
 	from urllib3 import make_headers
-	USER_AGENT = "vtAPI/1.0 (A Python interface to the public virustotal API)"
-	headers = make_headers(keep_alive=False, user_agent=USER_AGENT)
-	global _CONNECTION
-	if(_PROXYURL != ""):
-		from urllib3 import ProxyManager
-		_CONNECTION = urllib3.ProxyManager(proxy_url=_PROXYURL, headers=headers)
+	if(_USERAGENT==""):
+		print("The user agent has not been set.")
 	else:
-		_CONNECTION = urllib3.PoolManager(headers=headers)
+		headers = make_headers(keep_alive=False, user_agent=_USERAGENT)
+		global _CONNECTION
+		if(_PROXYURL != ""):
+			from urllib3 import ProxyManager
+			_CONNECTION = urllib3.ProxyManager(proxy_url=_PROXYURL, headers=headers)
+		else:
+			_CONNECTION = urllib3.PoolManager(headers=headers)
 
 def setProxy(p_proxyurl):
 	pattern = """(?:http|https)(?:\:\/\/)(?:[a-z]*(?:\.)?){5}(?:\:[0-9]{1,5})"""
@@ -62,6 +71,10 @@ def setProxy(p_proxyurl):
 	else:
 		print("The provided proxy url is invalid.")
 
+def setUserAgent(p_useragent):
+	global _USERAGENT
+	_USERAGENT = p_useragent
+
 def setApiKey(p_apikey):
 	pattern = """[a-z0-9]{64}"""
 	if(re.match(pattern, p_apikey)):
@@ -70,6 +83,7 @@ def setApiKey(p_apikey):
 	else:
 		print("The provided api key is invalid.")
 
+# Request functions
 def scanUrl(p_url):
 	"""
 	Sends the url in 'p_url' to virustotal
@@ -80,7 +94,7 @@ def scanUrl(p_url):
 		if(_CONNECTION == None):
 			raise ConnectionError
 		else:
-			baseurl = "https://www.virustotal.com/vtapi/v2/url/report"
+			baseurl = _BASE_APIURL+"url/report"
 			return _sendArtifact(baseurl, p_url)
 	except ConnectionError as e:
 		print(e.message)
@@ -96,7 +110,7 @@ def scanFile(p_filehash):
 		if(_CONNECTION == None):
 			raise ConnectionError
 		else:
-			baseurl = "https://www.virustotal.com/vtapi/v2/file/report"
+			baseurl = _BASE_APIURL+"file/report"
 			return _sendArtifact(baseurl, p_filehash)
 	except ConnectionError as e:
 		print(e.message)
